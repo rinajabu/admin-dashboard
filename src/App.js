9deaff74a2f871ebb16e0303f6708cdbac497fba
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro-2.local>
2023-01-09 10:51:04 -0500

    theme provider and light/dark mode setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
-	return <div className="app"></div>;
+	const [theme, colorMode] = useMode();
+
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className="app">
+					<main className="content">
+						<h1>Admin Dashboard</h1>
+					</main>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
 }
 
 export default App;
---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Topbar = () => {
+  return (
+    <div>Topbar</div>
+  )
+}
+
+export default Topbar
