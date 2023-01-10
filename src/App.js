07a5207577f2b52bcf16e96419f45dba291b2b9c
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro-2.local>
2023-01-10 10:25:39 -0500

    form and calendar completed

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
+import Form from "./scenes/form";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/calendar";
 
 function App() {
 	const [theme, colorMode] = useMode();
@@ -31,13 +31,13 @@ function App() {
 							<Route path="/team" element={<Team />} />
 							<Route path="/invoices" element={<Invoices />} />
 							<Route path="/contacts" element={<Contacts />} />
+							<Route path="/form" element={<Form />} />
 							{/* <Route path="/bar" element={<Bar />} /> */}
-							{/* <Route path="/form" element={<Form />} /> */}
 							{/* <Route path="/line" element={<Line />} /> */}
 							{/* <Route path="/pie" element={<Pie />} /> */}
 							{/* <Route path="/faq" element={<FAQ />} /> */}
 							{/* <Route path="/geography" element={<Geography />} /> */}
-							{/* <Route path="/calendar" element={<Calendar />} /> */}
+							<Route path="/calendar" element={<Calendar />} />
 						</Routes>
 					</main>
 				</div>
