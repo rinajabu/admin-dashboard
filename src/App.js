1d8a45d1d54bdb5d0f94c141a9ffe7c48316292b
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro-2.local>
2023-01-09 21:45:53 -0500

    data tables for team, contacts, and invoices pages complete

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -28,9 +28,9 @@ function App() {
 						<Topbar />
 						<Routes>
 							<Route path="/" element={<Dashboard />} />
-							{/* <Route path="/team" element={<Team />} /> */}
-							{/* <Route path="/invoices" element={<Invoices />} /> */}
-							{/* <Route path="/contacts" element={<Contacts />} /> */}
+							<Route path="/team" element={<Team />} />
+							<Route path="/invoices" element={<Invoices />} />
+							<Route path="/contacts" element={<Contacts />} />
 							{/* <Route path="/bar" element={<Bar />} /> */}
 							{/* <Route path="/form" element={<Form />} /> */}
 							{/* <Route path="/line" element={<Line />} /> */}
